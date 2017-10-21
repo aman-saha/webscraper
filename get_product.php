0eb11baf5bace78e72ebb5368e49a5dac9216dfd
aman-saha <aman.saha2015@vit.ac.in>
2017-10-22 00:33:27 +0530

    automated fteching reviews

--- get_product.php
+++ get_product.php
@@ -1,11 +1,11 @@
 <?php
     @ob_start();
-    include("../includes/database_connection.php");
-    include("../includes/session.php");
-    include("../includes/functions.php");
+    include("includes/database_connection.php");
+    include("includes/session.php");
+    include("includes/functions.php");
 ?>
 <?php
-	$data = file_get_contents("data.json");
+	/*$data = file_get_contents("data.json");
 	$json_data = json_decode($data, true);
 	$e = count($json_data);
 	echo $e;
@@ -17,5 +17,21 @@
 	}
 	//echo $json_data[0]['reviews'][0]['review_header'];
 	echo $c;
-	//print_r($json_data);
+	//print_r($json_data);*/
+?>
+<?php
+	// used to extract reviews from Amazon
+	$filename = escapeshellcmd("Amazon-Review-Scraper/scrape_product.py");
+    //chmod($filename,0777);
+    $command = "python " . $filename;
+    $output = shell_exec($command);
+    echo $output;
+
+    // used to extract reviews from Amazon
+	$filename = escapeshellcmd("Flipkart-Review-Scraper/scrape_product.py");
+    //chmod($filename,0777);
+    $command = "python " . $filename;
+    $output = shell_exec($command);
+    echo $output;
+
 ?>
