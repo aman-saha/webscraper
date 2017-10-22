d3fe60301f5801620f017e2fb48559ba68dab50e
aman-saha <aman.saha2015@vit.ac.in>
2017-10-22 10:44:40 +0530

    analyzer

--- sentiment-analyzer/src/test.php
+++ sentiment-analyzer/src/test.php
@@ -34,45 +34,6 @@
 	$sat->trainAnalyzer('../trainingSet/data.neg', 'negative', 5000); //training with negative data
 	$sat->trainAnalyzer('../trainingSet/data.pos', 'positive', 5000); //trainign with positive data
 
-
-	/*
-		The analyzeSentence method accepts as a sentence as parameter and score it as a positive, 
-		negative or neutral sentiment. it returns an array that looks like this:
-
-		array
-		(
-			'sentiment' => '[the sentiment value returned]',
-			'accuracy' => array
-							(
-								'positivity'=> 'A floating point number showing us the probability of the sentence being positive',
-								'negativity' => 'A floating point number showing us the probability of the sentence being negative',
-							),
-		)
-
-		An example is shown below:
-	*/
-
-/*		$sentence1 = 'while the performances are often engaging , this loose collection of largely improvised numbers would probably have worked better as a one-hour tv documentary . ';
-		$sentence2 = 'edited and shot with a syncopated style mimicking the work of his subjects , pray turns the idea of the documentary on its head , making it rousing , invigorating fun lacking any mtv puffery . 
-';
-
-		$sentimentAnalysisOfSentence1 = $sat->analyzeSentence($sentence1);
-
-		$resultofAnalyzingSentence1 = $sentimentAnalysisOfSentence1['sentiment'];
-		$probabilityofSentence1BeingPositive = $sentimentAnalysisOfSentence1['accuracy']['positivity'];
-		$probabilityofSentence1BeingNegative = $sentimentAnalysisOfSentence1['accuracy']['negativity'];
-
-		$sentimentAnalysisOfSentence2 = $sat->analyzeSentence($sentence2);
-
-		$resultofAnalyzingSentence2 = $sentimentAnalysisOfSentence2['sentiment'];
-		$probabilityofSentence2BeingPositive = $sentimentAnalysisOfSentence2['accuracy']['positivity'];
-		$probabilityofSentence2BeingNegative = $sentimentAnalysisOfSentence2['accuracy']['negativity'];*/
-
-
-
-		
-
-
 	/*
 		The AnalyzeDocument method accepts the path to a text file as parameter.
 		It analyzes the file and scores it as either a positive or a negative sentiment. It also
